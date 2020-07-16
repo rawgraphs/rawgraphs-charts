f98f268e36f0ca159281fd3d361708faee1e24fd
Mauro Bianchi <bianchimro@gmail.com>
2020-07-16 11:17:58 +0200

    added movies dataset and second scatterplot example

--- example/configurations/scatterplot2.js
+++ example/configurations/scatterplot2.js
@@ -1,5 +1,5 @@
 import scatterplot from "rawcharts/scatterplot"
-import data from "../datasets/Titanic.tsv"
+import data from "../datasets/Movies.tsv"
 
 console.log(data)
 
@@ -7,19 +7,22 @@ export default {
   chart: scatterplot,
   data,
   dataTypes: {
-    Age: "number",
-    Fare: "number",
+    Year: "number",
+    "Runtime (minutes)": "number",
+    Rating: "number",
+    Title: "string",
+    Genre: "string",
   },
   mapping: {
-    x: { value: ["Age"] },
-    y: { value: ["Fare"] },
-    color: { value: ["Age"] },
-    label: { value: ["Fare"] },
-    size: { value: ["Age"] },
+    x: { value: ["Year"] },
+    y: { value: ["Runtime (minutes)"] },
+    color: { value: ["Genre"] },
+    label: { value: ["Title"] },
+    size: { value: ["Rating"] },
   },
   visualOptions: {
     width: 500,
     height: 500,
-    xOrigin: true,
+    xOrigin: false,
   },
 }
