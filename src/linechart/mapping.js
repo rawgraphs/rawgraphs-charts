49f943ffff7783e6221386b0998110038bd61054
mikima <mikimauri+github@gmail.com>
2020-08-19 11:04:14 +0200

    linechart: mapping now exports flat dataset. updated render
    * mapping now returns a flat dataset, gathered using hte new d3-array rollup function.
    * nested structure is created now inside the render
    * updated render to work with the new structure
    * Added a new configuration

--- src/linechart/mapping.js
+++ src/linechart/mapping.js
@@ -3,21 +3,29 @@ import * as d3Array from 'd3-array'
 
 export const mapData = function(data, mapping, dataTypes, dimensions) {
 
-  console.log({data, mapping, dataTypes, dimensions})
+  //console.log({data, mapping, dataTypes, dimensions})
+
+	let results = [];
 
   const result = d3Array.rollups(data,
     v => {
-      return {
-        'x': v[0][mapping.x.value], //get the first one since it's grouped
-        'y': d3.sum(v, d => d[mapping.y.value]),
-        'color': [...new Set(v.map(d => d[mapping.color.value]))].join(','), // TODO: not working if X axis is mapped also as color
-        'series': v[0][mapping.series.value], //get the first one since it's grouped
-        'lines': v[0][mapping.lines.value] //get the first one since it's grouped
-      }
+      return d3Array.rollups(v,
+				vv => {
+					const item = {
+							'x': vv[0][mapping.x.value], //get the first one since it's grouped
+        			'y': d3.sum(vv, d => d[mapping.y.value]),
+							'color':[...new Set(v.map(d => d[mapping.color.value]))].join(','),
+							'series': v[0][mapping.series.value],
+							'lines': v[0][mapping.lines.value]
+						};
+					results.push(item);
+					},
+				d => d[mapping.x.value].toString() // sub-group functions
+			)
     },
     d => d[mapping.series.value], // series grouping
-    d => d[mapping.lines.value], d => d[mapping.x.value].toString()// group functions
+    d => d[mapping.lines.value] // group functions
   )
 
-  return result
+  return results
 }
