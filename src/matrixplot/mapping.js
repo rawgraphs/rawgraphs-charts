5b1e3d0b71f74a2f2da2a9ed7fe0d45a0493e0cc
mikima <mikimauri+github@gmail.com>
2020-09-04 09:44:53 +0200

    [matrixplot, sunburst] fixed aggregation when size is not set
    Not it returns the basic count for both

--- src/matrixplot/mapping.js
+++ src/matrixplot/mapping.js
@@ -17,7 +17,7 @@ export const mapData = function(data, mapping, dataTypes, dimensions) {
       return {
         'x': v[0][mapping.x.value], // get the first one since it's grouped
         'y': v[0][mapping.y.value], // get the first one since it's grouped
-        'size': mapping.size.value ? sizeAggregator(v.map(d => d[mapping.size.value])) : 1, // aggregate. If not mapped, give 1 as size
+        'size': mapping.size.value ? sizeAggregator(v.map(d => d[mapping.size.value])) : v.length, // aggregate. If not mapped, give 1 as size
         'color': mapping.color.value ? colorAggregator(v.map(d => d[mapping.color.value])) : 'default', // aggregate, by default single color.
         'label': mapping.label.value ? mapping.label.value.map((label,i) =>{return labelAggregators[i](v.map(d =>d[label]))}) : undefined // create array of strings
       }
--- src/sunburst/mapping.js
+++ src/sunburst/mapping.js
@@ -18,7 +18,7 @@ export const mapData = function(data, mapping, dataTypes, dimensions) {
     v => {
       const item = {
         'hierarchy': new Map(mapping.hierarchy.value.map(d => [d,v[0][d]])), //get the first one since it's grouped
-        'size': mapping.size.value ? sizeAggregator(v.map(d => d[mapping.size.value])) : 1,
+        'size': mapping.size.value ? sizeAggregator(v.map(d => d[mapping.size.value])) : v.length,
         'color': mapping.color.value ? colorAggregator(v.map(d => d[mapping.color.value])) : 'cells color',
         'label': mapping.label.value ? mapping.label.value.map((label,i) =>{return labelAggregators[i](v.map(d =>d[label]))}) : undefined // create array of strings
       }
