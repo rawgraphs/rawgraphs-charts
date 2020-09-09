9c32cf5fe480b924254104b72bcca0f74822440b
mikima <mikimauri+github@gmail.com>
2020-09-09 10:55:17 +0200

    [circlepacking] bootstrapped chart

---
+++ example/configurations/circlepacking1.js
@@ -0,0 +1,34 @@
+import circlepacking from "rawcharts/circlepacking"
+import data from "../datasets/WineTasting.tsv"
+
+export default {
+  chart: circlepacking,
+  data,
+  dataTypes: {
+	  "Root": "string",
+	  "Level1": "string",
+	  "Level2": "string",
+	  "Level3": "string",
+	  "results": "number"
+	},
+  mapping: {
+    hierarchy: { value: ["Root", "Level1", "Level2", "Level3"] },
+		color: {
+			value: ["Root"],
+			config: {"aggregation": ["csvDistinct"]}
+		},
+    label: {
+			value: ["Level1", "results", "Root"],
+			config: {"aggregation": ["csvDistinct","sum","csvDistinct"]}
+		},
+    size: {
+			value: ["results"],
+			config: {"aggregation": ["sum"]}
+		},
+  },
+  visualOptions: {
+    width: 500,
+    height: 500,
+		tiling: "Slice and dice"
+  },
+}
---
+++ src/circlepacking/metadata.js
@@ -0,0 +1,10 @@
+export const metadata =  {
+  name: 'Treemap',
+  //#TODO: CHECK ROLLUP OPTIONS FOR LOADING IMAGES (thumb and icon)
+  //thumbnail:"https://via.placeholder.com/320x200/4bc06a/999999.png?text=Scatter%20Plot%20Thumb",
+  // icon,
+  category: '',
+  description: 'Slice slice slice',
+  // code:'https://github.com/rawgraphs/raw',
+  // tutorial:'https://rawgraphs.io/learning/'
+}
