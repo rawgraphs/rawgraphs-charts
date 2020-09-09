433b9108a29c485916c2f67981458d994df68943
mikima <mikimauri+github@gmail.com>
2020-09-09 15:56:35 +0200

    bootstrapped dendrogram

---
+++ src/dendrogram/visualOptions.js
@@ -0,0 +1,42 @@
+export const visualOptions = {
+
+  marginTop : {
+    type: 'number',
+    label: 'Margin (top)',
+    default: 10,
+    group: 'artboard',
+  },
+
+  marginRight : {
+    type: 'number',
+    label: 'Margin (right)',
+    default: 10,
+    group: 'artboard',
+  },
+
+  marginBottom : {
+    type: 'number',
+    label: 'Margin (bottom)',
+    default: 10,
+    group: 'artboard',
+  },
+
+  marginLeft : {
+    type: 'number',
+    label: 'Margin (left)',
+    default: 10,
+    group: 'artboard',
+  },
+
+  colorScale : {
+    type: 'colorScale',
+    label: 'The color scale',
+    dimension: 'color',
+    default: {
+      scaleType: 'ordinal',
+      interpolator: 'interpolateSpectral'
+    },
+    group: 'chart'
+  },
+
+}
