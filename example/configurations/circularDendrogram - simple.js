18505da93727a596ffcf9369a96976f847c0be56
mikima <mikimauri+github@gmail.com>
2020-09-17 16:09:34 +0200

    bootstrapped circular dendrogram

---
+++ example/configurations/circularDendrogram - simple.js
@@ -0,0 +1,28 @@
+import circularDendrogram from "rawcharts/circularDendrogram"
+import data from "../datasets/simple-hierarchy.tsv"
+
+export default {
+  chart: circularDendrogram,
+  data,
+  dataTypes: {
+	  "Level1": "string",
+	  "Level2": "string",
+	  "Size": "number"
+	},
+  mapping: {
+    hierarchy: { value: ["Level1", "Level2"] },
+    color: {
+			value: ["Level1"],
+			config: {"aggregation": ["csvDistinct"]}
+		},
+		size: {
+			value: ["Size"],
+			config: {"aggregation": ["sum"]}
+		},
+  },
+  visualOptions: {
+    width: 500,
+    height: 500,
+		// layout: 'Cluster Dendogram',
+  },
+}
---
+++ src/circularDendrogram/metadata.js
@@ -0,0 +1,10 @@
+export const metadata =  {
+  name: 'Circular dendrogram',
+
+  //thumbnail:"https://via.placeholder.com/320x200/4bc06a/999999.png?text=Scatter%20Plot%20Thumb",
+  // icon,
+  category: 'Hierarchy',
+  description: '',
+  // code:'https://github.com/rawgraphs/raw',
+  // tutorial:'https://rawgraphs.io/learning/'
+}
