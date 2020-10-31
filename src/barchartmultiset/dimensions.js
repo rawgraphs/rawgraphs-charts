f10e01a993743279ce4348270cc7a83b0b6fd311
mikima <mikimauri+github@gmail.com>
2020-10-31 01:25:40 +0100

    [bar chart multiset] bootstrapped

---
+++ src/barchartmultiset/dimensions.js
@@ -0,0 +1,49 @@
+export const dimensions = [
+  {
+    id: 'bars',
+    name: 'bars',
+    validTypes: ['number', 'string', 'date'],
+    required: true,
+    operation: 'get',
+  },
+
+  {
+    id: 'sets',
+    name: 'sets',
+    validTypes: ['number', 'string', 'date'],
+    required: true,
+    operation: 'get',
+  },
+
+  {
+    id: 'size',
+    name: 'size',
+    operation: 'get',
+    validTypes: ['number'],
+    required: false,
+    aggregation: true,
+    aggregationDefault: 'sum',
+  },
+
+  {
+    id: 'color',
+    name: 'color',
+    operation: 'get',
+    validTypes: ['number', 'string', 'date'],
+    required: false,
+    aggregation: true,
+    aggregationDefault: {
+      number: 'sum',
+      string: 'csvDistinct',
+      date: 'csvDistinct',
+    },
+  },
+
+  {
+    id: 'series',
+    name: 'series',
+    validTypes: ['number', 'string', 'date'],
+    required: false,
+    operation: 'get',
+  },
+]
---
+++ src/barchartmultiset/metadata.js
@@ -0,0 +1,13 @@
+import icon from './barchart.svg'
+import thumbnail from './barchart_thumb.svg'
+
+export const metadata = {
+  name: 'Multi-set bar chart',
+  thumbnail,
+  icon,
+  categories: ['correlations'],
+  description:
+    'It displays a categorical dimension and related amounts. Each bar represents a category, width is proportional to the quantitative dimension.',
+  // code: "https://github.com/rawgraphs/raw",
+  // tutorial: "https://rawgraphs.io/learning/",
+}
