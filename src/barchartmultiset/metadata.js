a8a8a43bf8d143d0b5eb7be661d0e8140de4d396
mikima <mikimauri+github@gmail.com>
2020-10-31 15:21:51 +0100

    [barchart multiset] Updated metadata
    barchart multiset:
    * added icon and thumbnail
    * updated description

--- src/barchartmultiset/metadata.js
+++ src/barchartmultiset/metadata.js
@@ -1,13 +1,13 @@
-import icon from './barchart.svg'
-import thumbnail from './barchart_thumb.svg'
+import icon from './barchartmultiset.svg'
+import thumbnail from './barchartmultiset_thumb.svg'
 
 export const metadata = {
   name: 'Multi-set bar chart',
   thumbnail,
   icon,
-  categories: ['correlations'],
+  categories: ['correlations', 'proportions'],
   description:
-    'It displays a categorical dimension and related amounts. Each bar represents a category, width is proportional to the quantitative dimension.',
+    'It displays multiple quantitative dimensions related to categories. bars are visually grouped in sets according to the categorical dimension, each bar represents a quantitative dimension, mapped on its height.',
   // code: "https://github.com/rawgraphs/raw",
   // tutorial: "https://rawgraphs.io/learning/",
 }
