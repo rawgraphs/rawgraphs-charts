7710d6949f96132198c0325dd55eb4266c74e052
Saarthak <saarthakmaini@gmail.com>
2023-05-22 18:00:52 +0530

    Fix Typo

--- src/dendrogram/render.js
+++ src/dendrogram/render.js
@@ -103,7 +103,7 @@ export function render(
   // compute padding
   const padding = (chartHeight - totalValue) / (hierarchy.leaves().length - 1)
 
-  // dictionary to choose algorythm according to options
+  // dictionary to choose algorithm according to options
   const layouts = {
     'Cluster Dendogram': d3.cluster(),
     Tree: d3.tree(),
